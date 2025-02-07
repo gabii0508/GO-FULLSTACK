5e7fdbdc3a9a29fa5cf06797f8f21c3d190da10f
gabii0508 <gabygui27@gmail.com>
2025-02-07 09:32:34 +0100

    Create Users: Configure Authentication Routes

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 
 mongoose.connect('mongodb+srv://gaby:Gabriela30.@cluster0.yfra7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
@@ -23,6 +24,7 @@ app.use((req, res, next) => {
   app.use(bodyParser.json()); 
 
   app.use('/api/stuff', stuffRoutes);
+  app.use('/api/auth', userRoutes);
 
  
 module.exports = app;
